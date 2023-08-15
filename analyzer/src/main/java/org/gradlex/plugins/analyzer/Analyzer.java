0bed02b83b322ee1f8fa885825a8eae5c373e9f4
Lóránt Pintér <lorant@gradle.com>
2023-08-15 11:51:42 +0200

    Traverse type hierarchy

--- analyzer/src/main/java/org/gradlex/plugins/analyzer/Analyzer.java
+++ analyzer/src/main/java/org/gradlex/plugins/analyzer/Analyzer.java
@@ -2,7 +2,9 @@ package org.gradlex.plugins.analyzer;
 
 import sootup.core.Project;
 import sootup.core.inputlocation.AnalysisInputLocation;
+import sootup.core.types.ClassType;
 import sootup.java.bytecode.inputlocation.JavaClassPathAnalysisInputLocation;
+import sootup.java.core.JavaIdentifierFactory;
 import sootup.java.core.JavaProject;
 import sootup.java.core.JavaSootClass;
 import sootup.java.core.language.JavaLanguage;
@@ -12,6 +14,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Analyzer {
@@ -27,9 +30,25 @@ public class Analyzer {
 
         Project<JavaSootClass, JavaView> project = JavaProject.builder(language).addInputLocation(inputLocation).build();
         JavaView view = project.createView();
-        view.getClasses().forEach(clazz -> {
-            System.out.println(clazz);
-        });
+
+        ClassType classType = JavaIdentifierFactory.getInstance().getClassType("com.vaadin.gradle.VaadinCleanTask");
+        while (true) {
+            Optional<JavaSootClass> foundClass = view.getClass(classType);
+            if (!foundClass.isPresent()) {
+                System.out.println("Cannot find class " + classType);
+                break;
+            }
+            JavaSootClass clazz = foundClass.get();
+            System.out.println("Found class: " + clazz);
+            Optional<? extends ClassType> superclass = clazz.getSuperclass();
+            if (superclass.isPresent()) {
+                classType = superclass.get();
+            } else {
+                System.out.println("No superclass present");
+                break;
+            }
+        }
+
         System.out.println("Stored classes: " + view.getAmountOfStoredClasses());
     }
 }
