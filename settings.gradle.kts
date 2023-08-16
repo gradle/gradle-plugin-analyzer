2320030e541b5f7a78aaa3f35a8fa336a9d1b61c
Lóránt Pintér <lorant@gradle.com>
2023-08-16 14:12:08 +0200

    Enable build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,15 @@ pluginManagement {
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+    id("com.gradle.enterprise") version "3.14.1"
+}
+
+gradleEnterprise {
+    buildScan {
+        publishAlways()
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
 }
 
 rootProject.name = "plugin-analyzer-runner"
@@ -14,3 +23,4 @@ include("runner")
 
 enableFeaturePreview(FeaturePreviews.Feature.TYPESAFE_PROJECT_ACCESSORS.name)
 enableFeaturePreview(FeaturePreviews.Feature.STABLE_CONFIGURATION_CACHE.name)
+
