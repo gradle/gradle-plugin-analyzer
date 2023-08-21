fdfc35ecb1f99581d72863c1d8a05e9420e77983
Lóránt Pintér <lorant@gradle.com>
2023-08-21 21:54:39 +0200

    Add tests for static Groovy

--- gradle-plugin-analyzer/analyzer/src/test/groovy/org/gradlex/plugins/analyzer/analysis/AbstractTaskImplementationDoesNotOverrideMethodTest.groovy
+++ gradle-plugin-analyzer/analyzer/src/test/groovy/org/gradlex/plugins/analyzer/analysis/AbstractTaskImplementationDoesNotOverrideMethodTest.groovy
@@ -1,11 +1,11 @@
 package org.gradlex.plugins.analyzer.analysis
 
-import com.google.common.base.Predicates
+import com.ibm.wala.classLoader.IMethod
 
 class AbstractTaskImplementationDoesNotOverrideMethodTest extends AbstractAnalysisSpec {
-    def analysis = new AbstractTaskImplementationDoesNotOverrideMethod("method", Predicates.alwaysTrue()) {}
+    def analysis = new AbstractTaskImplementationDoesNotOverrideMethod("method", { IMethod method -> !method.static && !method.init }) {}
 
-    def "can detect super-only overriding method"() {
+    def "can detect super-only overriding method in plain Java code"() {
         compileJava("""
             class CustomTask extends org.gradle.api.tasks.SourceTask {
                 @Override
@@ -30,7 +30,7 @@ class AbstractTaskImplementationDoesNotOverrideMethodTest extends AbstractAnalys
         ]
     }
 
-    def "can detect overriding method with custom logic"() {
+    def "can detect overriding method with custom logic in plain Java code"() {
         compileJava("""
             class CustomTask extends org.gradle.api.DefaultTask {               
                 @Override
@@ -47,6 +47,54 @@ class AbstractTaskImplementationDoesNotOverrideMethodTest extends AbstractAnalys
         then:
         reports == [
             "WARN: The method setEnabled() in LCustomTask overrides Gradle API from Lorg/gradle/api/DefaultTask with custom logic: Instruction #4 expected to be ReturnInstruction but found class com.ibm.wala.shrike.shrikeBT.GetInstruction\$Lazy"
+        ]
+    }
+
+    def "can detect super-only overriding method in static Groovy code"() {
+        compileGroovy("""
+            @groovy.transform.CompileStatic
+            class CustomTask extends org.gradle.api.tasks.SourceTask {
+                @Override
+                boolean getEnabled() {
+                    return super.getEnabled()
+                }
+
+                @Override
+                org.gradle.api.file.FileTree getSource() {
+                    return super.getSource()
+                }
+            }
+        """)
+
+        when:
+        analyzer.analyze(analysis)
+
+        then:
+        reports == [
+            "INFO: The method getEnabled() in LCustomTask overrides Gradle API from Lorg/gradle/api/DefaultTask, but calls only super()",
+            "INFO: The method getSource() in LCustomTask overrides Gradle API from Lorg/gradle/api/tasks/SourceTask, but calls only super()",
+        ]
+    }
+
+
+    def "can detect overriding method with custom logic in static Groovy code"() {
+        compileGroovy("""
+            @groovy.transform.CompileStatic
+            class CustomTask extends org.gradle.api.DefaultTask {               
+                @Override
+                void setEnabled(boolean value) {
+                    super.setEnabled(value)
+                    println "Additional logic"
+                }
+            }
+        """)
+
+        when:
+        analyzer.analyze(analysis)
+
+        then:
+        reports == [
+            "WARN: The method setEnabled() in LCustomTask overrides Gradle API from Lorg/gradle/api/DefaultTask with custom logic: Instruction #4 expected to be ReturnInstruction but found class com.ibm.wala.shrike.shrikeBT.ConstantInstruction\$ConstNull"
         ]
     }
 }
